897bc77e3f4a682550e93a50ab154c618200a7fb
Mike Evans <mike.evans88@gmail.com>
2016-03-19 12:14:02 -0400

    css changes while I work on max width

--- js/app.js
+++ js/app.js
@@ -13,9 +13,11 @@ angular.module('website', ['ngAnimate'])
             {image: '../KristinLaurelCaffray/img/OccupyBoston/Caffray_110930-2258.jpg', description: 'Image 2258'}
         ];
 
+        $scope.direction = 'left';
         $scope.currentIndex = 0;
 
         $scope.setCurrentSlideIndex = function (index) {
+            $scope.direction = (index > $scope.currentIndex) ? 'left' : 'right';
             $scope.currentIndex = index;
         };
 
@@ -24,33 +26,47 @@ angular.module('website', ['ngAnimate'])
         };
 
         $scope.prevSlide = function () {
+            $scope.direction = 'left';
             $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
         };
 
         $scope.nextSlide = function () {
+            $scope.direction = 'right';
             $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
         };
     })
     .animation('.slide-animation', function () {
       return {
-          addClass: function (element, className, done) {
+          beforeAddClass: function (element, className, done) {
+              var scope = element.scope();
+
               if (className == 'ng-hide') {
-                TweenMax.to(element, 0.5, {left: -element.parent().width(), onComplete: done });
-              }
-              else {
+                    var finishPoint = element.parent().width();
+                    if(scope.direction !== 'right') {
+                        finishPoint = -finishPoint;
+                    }
+                    TweenMax.to(element, 0.5, {left: finishPoint, onComplete: done });
+                }
+                else {
                   done();
-              }
+                }
           },
           removeClass: function (element, className, done) {
-              if (className == 'ng-hide') {
-                element.removeClass('ng-hide');
+                var scope = element.scope();
 
-                TweenMax.set(element, { left: element.parent().width() });
-                TweenMax.to(element, 0.5, {left: 0, onComplete: done });
-              }
-              else {
+                if (className == 'ng-hide') {
+                    element.removeClass('ng-hide');
+
+                    var startPoint = element.parent().width();
+                    if(scope.direction === 'right') {
+                        startPoint = -startPoint;
+                    }
+
+                    TweenMax.fromTo(element, 0.5, { left: startPoint }, {left: 0, onComplete: done });
+                }
+                else {
                   done();
-              }
+                }
           }
       };
     });
