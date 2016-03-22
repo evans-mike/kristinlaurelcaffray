9973afbde4bef67622a0ade2701fb1c933349852
Mike Evans <mike.evans88@gmail.com>
2016-03-22 18:13:10 -0400

    fixed omg max-height

--- js/interactive.js
+++
@@ -1,8 +0,0 @@
-function set_body_width() { // set body height = window height
-  var wh = $(window).width();
-  $(".slider").width(wh);
-}
-$(document).ready(function() {
-  set_body_width();
-  $(window).bind('resize', function() { set_body_width(); });
-});
