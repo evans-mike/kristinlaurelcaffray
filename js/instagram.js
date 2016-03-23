3d24ca4d3cea69a653b96f03b1289633247e59ac
Mike Evans <mike.evans88@gmail.com>
2016-03-23 08:20:57 -0400

    working on instagram API son

--- js/instagram.js
+++ js/instagram.js
@@ -1,7 +1,30 @@
-$(document).ready(function() {
-  $('.instagra_blog').load(function(){
-    var instagramAPI = "https://api.instagram.com/?jsoncallback=?";
-    var instagramOptions = '';
-    $.getJSON()
+// $(document).ready(function() {
+//   $('.instagra_blog').load(function(){
+//     var instagramAPI = "https://api.instagram.com/?jsoncallback=?";
+//     var instagramOptions = {
+//       author_id: 329396167,
+//       format: "json"
+//     };
+//     var displayPhotos(data){
+//
+//     };
+//     $.getJSON()
+//   });
+// });//end ready
+//
+//
+// https://api.instagram.com/v1/users/yeahokayok/media/recent/?access_token=458814967.b7572ad.f759e9bc61a5439a84fc455fc84dd503
+
+
+$.getJSON("https://api.instagram.com/v1/recent?access_token=458814967.b7572ad.f759e9bc61a5439a84fc455fc84dd503&callback=callbackFunction",
+  {
+    author_id: 458814967
+  },
+  function (data){
+    var photoHTML = '<ul>';
+    $.each(data.items, function(i, photo){
+      photoHTML += 'photo.html';
+    });
+    photoHTML += '</ul>';
+    $('#photos').html(photoHTML);
   });
-});//end ready
