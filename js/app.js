110163e08c2587b0a2e854fb29e81f802f36fe1d
Mike Evans <mike.evans88@gmail.com>
2016-03-23 16:14:50 -0400

    img links

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 angular.module('website', ['ngAnimate'])
     .controller('MainCtrl', function ($scope) {
       $scope.slides = [
-            {image: '../img/OccupyBoston/Caffray_110930-1812.jpg', description: 'Image 1812'},
-            {image: '../img/OccupyBoston/Caffray_110930-1821.jpg', description: 'Image 1821'},
-            {image: '../img/OccupyBoston/Caffray_110930-1829.jpg', description: 'Image 1829'},
-            {image: '../img/OccupyBoston/Caffray_110930-1860.jpg', description: 'Image 1860'},
-            {image: '../img/OccupyBoston/Caffray_110930-1898.jpg', description: 'Image 1898'},
-            {image: '../img/OccupyBoston/Caffray_110930-2090.jpg', description: 'Image 2090'},
-            {image: '../img/OccupyBoston/Caffray_110930-2222.jpg', description: 'Image 2222'},
-            {image: '../img/OccupyBoston/Caffray_110930-2240.jpg', description: 'Image 2240'},
-            {image: '../img/OccupyBoston/Caffray_110930-2255.jpg', description: 'Image 2255'},
-            {image: '../img/OccupyBoston/Caffray_110930-2258.jpg', description: 'Image 2258'}
+            {image: '../kristinlaurelcaffray/img/OccupyBoston/Caffray_110930-1812.jpg', description: 'Image 1812'},
+            {image: '../kristinlaurelcaffray/img/OccupyBoston/Caffray_110930-1821.jpg', description: 'Image 1821'},
+            {image: '../kristinlaurelcaffray/img/OccupyBoston/Caffray_110930-1829.jpg', description: 'Image 1829'},
+            {image: '../kristinlaurelcaffray/img/OccupyBoston/Caffray_110930-1860.jpg', description: 'Image 1860'},
+            {image: '../kristinlaurelcaffray/img/OccupyBoston/Caffray_110930-1898.jpg', description: 'Image 1898'},
+            {image: '../kristinlaurelcaffray/img/OccupyBoston/Caffray_110930-2090.jpg', description: 'Image 2090'},
+            {image: '../kristinlaurelcaffray/img/OccupyBoston/Caffray_110930-2222.jpg', description: 'Image 2222'},
+            {image: '../kristinlaurelcaffray/img/OccupyBoston/Caffray_110930-2240.jpg', description: 'Image 2240'},
+            {image: '../kristinlaurelcaffray/img/OccupyBoston/Caffray_110930-2255.jpg', description: 'Image 2255'},
+            {image: '../kristinlaurelcaffray/img/OccupyBoston/Caffray_110930-2258.jpg', description: 'Image 2258'}
         ];
 
         $scope.direction = 'left';
