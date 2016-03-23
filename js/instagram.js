37d9e7b3ff4898939cd31cf8389b352b385725c7
Mike Evans <mike.evans88@gmail.com>
2016-03-22 22:30:26 -0400

    begin Instagram API coding

---
+++ js/instagram.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('.instagra_blog').load(function(){
+    var instagramAPI = "https://api.instagram.com/?jsoncallback=?";
+    var instagramOptions = '';
+    $.getJSON()
+  });
+});//end ready
